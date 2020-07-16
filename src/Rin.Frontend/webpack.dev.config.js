5e6ec98dfdd27444ee7fadcf95bc93ccc471080e
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-07-17 01:29:09 +0900

    chore: Configure History API fallback option

--- src/Rin.Frontend/webpack.dev.config.js
+++ src/Rin.Frontend/webpack.dev.config.js
@@ -2,6 +2,12 @@ const webpackMerge = require('webpack-merge');
 const configBase = require('./webpack.config.js');
 
 module.exports = webpackMerge.merge(configBase, {
+    output: {
+        publicPath: '/',
+    },
     mode: 'development',
     devtool: 'inline-source-map',
+    devServer: {
+        historyApiFallback: true
+    }
 });
