b32ed48d21675c5df1a84fe571209f67bf95848d
Mayuki Sawatari <mayuki+github@misuzilla.org>
2020-08-14 00:35:32 +0900

    fix: build error

--- src/Rin/Channel/HubInvoker/InvokerHelper.cs
+++ src/Rin/Channel/HubInvoker/InvokerHelper.cs
@@ -11,13 +11,13 @@ namespace Rin.Channel.HubInvoker
         private static readonly MethodInfo _methodInfoInvokeCoreTaskAsync = typeof(InvokerHelper<T>).GetMethods(BindingFlags.NonPublic | BindingFlags.Static).First(x => x.Name == "InvokeCoreTaskAsync" && x.GetGenericArguments().Length == 1)!;
         private static readonly MethodInfo _methodInfoInvokeCoreValueTaskAsync = typeof(InvokerHelper<T>).GetMethods(BindingFlags.NonPublic | BindingFlags.Static).First(x => x.Name == "InvokeCoreValueTaskAsync" && x.GetGenericArguments().Length == 1)!;
 
-        public static Func<object, InvokeMessage, ValueTask<InvocationResult>> GetInvoker(HubMethodDefinition methodDefinition)
+        public static Func<object, HubInvokeMessage, ValueTask<HubInvocationResult>> GetInvoker(HubMethodDefinition methodDefinition)
         {
             var func = GetInvokerCore(methodDefinition);
             return (instance, invokeMessage) => func((T)instance, invokeMessage);
         }
 
-        private static Func<T, InvokeMessage, ValueTask<InvocationResult>> GetInvokerCore(HubMethodDefinition methodDefinition)
+        private static Func<T, HubInvokeMessage, ValueTask<HubInvocationResult>> GetInvokerCore(HubMethodDefinition methodDefinition)
         {
             var returnType = methodDefinition.ReturnType;
             if (returnType == typeof(Task))
@@ -34,7 +34,7 @@ namespace Rin.Channel.HubInvoker
                 var actualReturnType = returnType.GetGenericArguments()[0];
                 if (openGenericType == typeof(Task<>) || openGenericType == typeof(ValueTask<>))
                 {
-                    Expression<Func<T, InvokeMessage, ValueTask<InvocationResult>>> expression = (instance, invokeMessage) => default;
+                    Expression<Func<T, HubInvokeMessage, ValueTask<HubInvocationResult>>> expression = (instance, invokeMessage) => default;
 
                     var methodCore = openGenericType == typeof(Task<>) ? _methodInfoInvokeCoreTaskAsync : _methodInfoInvokeCoreValueTaskAsync;
 
@@ -50,34 +50,34 @@ namespace Rin.Channel.HubInvoker
             return InvokeCore;
         }
 
-        private static ValueTask<InvocationResult> InvokeCore(T instance, InvokeMessage invokeMessage)
+        private static ValueTask<HubInvocationResult> InvokeCore(T instance, HubInvokeMessage invokeMessage)
         {
             var result = invokeMessage.MethodDefinition.Method.Invoke(instance, invokeMessage.Arguments);
-            return new ValueTask<InvocationResult>(new InvocationResult(result));
+            return new ValueTask<HubInvocationResult>(new HubInvocationResult(result));
         }
 
-        private static async ValueTask<InvocationResult> InvokeCoreValueTaskAsync<TResult>(T instance, InvokeMessage invokeMessage)
+        private static async ValueTask<HubInvocationResult> InvokeCoreValueTaskAsync<TResult>(T instance, HubInvokeMessage invokeMessage)
         {
             var task = (ValueTask<TResult>)invokeMessage.MethodDefinition.Method.Invoke(instance, invokeMessage.Arguments)!;
-            return new InvocationResult(await task);
+            return new HubInvocationResult(await task);
         }
 
-        private static async ValueTask<InvocationResult> InvokeCoreValueTaskAsync(T instance, InvokeMessage invokeMessage)
+        private static async ValueTask<HubInvocationResult> InvokeCoreValueTaskAsync(T instance, HubInvokeMessage invokeMessage)
         {
             await ((ValueTask)invokeMessage.MethodDefinition.Method.Invoke(instance, invokeMessage.Arguments)!);
-            return new InvocationResult();
+            return new HubInvocationResult();
         }
 
-        private static async ValueTask<InvocationResult> InvokeCoreTaskAsync<TResult>(T instance, InvokeMessage invokeMessage)
+        private static async ValueTask<HubInvocationResult> InvokeCoreTaskAsync<TResult>(T instance, HubInvokeMessage invokeMessage)
         {
             var task = ((Task<TResult>)invokeMessage.MethodDefinition.Method.Invoke(instance, invokeMessage.Arguments)!);
-            return new InvocationResult(await task);
+            return new HubInvocationResult(await task);
         }
 
-        private static async ValueTask<InvocationResult> InvokeCoreTaskAsync(T instance, InvokeMessage invokeMessage)
+        private static async ValueTask<HubInvocationResult> InvokeCoreTaskAsync(T instance, HubInvokeMessage invokeMessage)
         {
             await ((Task)invokeMessage.MethodDefinition.Method.Invoke(instance, invokeMessage.Arguments)!);
-            return new InvocationResult();
+            return new HubInvocationResult();
         }
     }
 }
